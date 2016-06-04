ab65d513d4bfe3d56be03aa504ae7c9bf1735233
Yash Gandhi <ysg410@gmail.com>
2016-06-04 08:33:37 +0530

    revert fixed

--- revert.sh
+++ revert.sh
@@ -1,20 +1,22 @@
 echo -n "Enter version number > "
 read text
+text1=text
+temp1=0
 cp db/$text.txt test.txt
 
 file="count.txt"
 while IFS= read line
 do
     temp1=$line
-	
+    i=$line	
 done <"$file"
 echo $text > "count.txt"
-i=temp1;
 
-while [ $i > $text ]
+while [ $i -gt $text ]
 do
-  rm -f db/$i.txt;
-  i=$(($i-1));
+   rm -f db/$i.txt
+  
+  i=$((i-1))
 done
 
 
